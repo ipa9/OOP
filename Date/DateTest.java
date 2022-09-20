fb8d566b5b839101b946f48b271285e2c8ac169c
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2022-09-20 21:39:01 +0400

    Update DateTest.java

--- Date/DateTest.java
+++ Date/DateTest.java
@@ -3,13 +3,5 @@ public class DateTest {
         Date newDate = new Date(9, 15, 2022);
         newDate.setMonth(10);
         newDate.displayDate();
-
-        try{
-            Date myDate = null;
-            myDate.setMonth(11);
-        }
-        catch(Exception e) {
-            System.out.println(e.toString());
-        }
     }
 }
